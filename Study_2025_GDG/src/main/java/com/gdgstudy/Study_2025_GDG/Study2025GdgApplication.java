abe1c6a170cc3a0028c25cc90d28242daaabea14
ruth1442 <ruth1442@naver.com>
2025-04-18 16:09:33 +0900

    initial commit

---
+++ Study_2025_GDG/src/main/java/com/gdgstudy/Study_2025_GDG/Study2025GdgApplication.java
@@ -0,0 +1,13 @@
+package com.gdgstudy.Study_2025_GDG;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Study2025GdgApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Study2025GdgApplication.class, args);
+	}
+
+}
